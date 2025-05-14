678d1de434928fa61fa86fc835663221f1cb17dd
Pierre Vanhove <pierre7159@gmail.com>
2025-05-14 13:46:25 -0400

    Add Cursor behaviour on theme

--- LettreMotivGenerator/obj/Debug/net8.0-windows/App.g.cs
+++ LettreMotivGenerator/obj/Debug/net8.0-windows/App.g.cs
@@ -48,7 +48,7 @@ namespace LettreMotivGenerator {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -74,7 +74,7 @@ namespace LettreMotivGenerator {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public static void Main() {
             LettreMotivGenerator.App app = new LettreMotivGenerator.App();
             app.InitializeComponent();
--- Logic/obj/Debug/net8.0/Logic.AssemblyInfo.cs
+++ Logic/obj/Debug/net8.0/Logic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b075e5fa9dc66f92f9e55c353fd21ad34214eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+161b7a5efb72350a05c989b8399523cb7b622911")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
